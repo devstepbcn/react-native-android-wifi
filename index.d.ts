b4fbfd5b05deea2b81acefc3d4c6c2f24813cde4
Phap <centralparkloop@yahoo.com>
2019-07-25 22:14:40 -0400

    add definition file

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+export interface wifi {
+    isEnabled: () => void;
+    setEnabled: () => void;
+    findAndConnect: () => void;
+    disconnect: () => void;
+    getSSID: () => void;
+    getBSSID: () => void;
+    loadWifiList: () => void;
+    connectionStatus: () => void;
+    getCurrentSignalStrength: () => void;
+    getFrequency: () => void;
+    getIP: () => void;
+    getDhcpServerAddress: () => void;
+    isRemoveWifiNetwork: () => void;
+    reScanAndLoadWifiList: () => void;
+    forceWifiUsage: () => void;
+    connectionStatusOfBoundNetwork: () => void;
+    connectToHiddenNetwork: () => void;
+}
